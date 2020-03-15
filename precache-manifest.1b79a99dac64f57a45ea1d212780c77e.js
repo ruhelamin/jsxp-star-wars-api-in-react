82130e6b7d9406193c45bbd4e7b68422fd916c16
Ruhel Amin <dev@ruhelamin.com>
2020-03-15 23:09:38 +0000

    Updates

---
+++ precache-manifest.1b79a99dac64f57a45ea1d212780c77e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc152a26a6e94eb63cde66c909262bc",
+    "url": "/jsxp-star-wars-api-in-react/index.html"
+  },
+  {
+    "revision": "49c5cd4217572c14671f",
+    "url": "/jsxp-star-wars-api-in-react/static/css/main.1837cdc4.chunk.css"
+  },
+  {
+    "revision": "c807c33ea325b4aa6270",
+    "url": "/jsxp-star-wars-api-in-react/static/js/2.89027ea3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/jsxp-star-wars-api-in-react/static/js/2.89027ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c5cd4217572c14671f",
+    "url": "/jsxp-star-wars-api-in-react/static/js/main.2210eda4.chunk.js"
+  },
+  {
+    "revision": "f04c28ee2947d824b4d9",
+    "url": "/jsxp-star-wars-api-in-react/static/js/runtime-main.8aae8359.js"
+  }
+]);
